b0bdbfa77b1b6d3424afb50480fe51b5c441ec19
jordanj0731 <jordanj0731@gmail.com>
2023-11-25 23:11:41 +0300

    'emissionInsight'

--- emissionInsight/src/app/app.component.ts
+++ emissionInsight/src/app/app.component.ts
@@ -1,112 +1,207 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Chart } from 'angular-highcharts';
 import { SensorDataService } from './aws-api.service';
+import { interval, Subscription } from 'rxjs';
+import * as Highcharts from 'highcharts';
+import HC_more from 'highcharts/highcharts-more';
+HC_more(Highcharts);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'emissionInsight';
   sensorData: any = {}; // Initialize an empty object to store sensor data
-
+  private updateSubscription!: Subscription;
+  carbonChart = new Chart(); // Assuming you are using the angular-highcharts library
+  methaneChart = new Chart(); // Assuming you are using the angular-highcharts library
   constructor(private sensorDataService: SensorDataService) {}
 
-  async ngOnInit() {
+
+  ngOnInit() {
+    this.setupGaugeCharts();
+    this.updateData();
+    this.updateSubscription = interval(600000000).subscribe(
+      (val) => { this.updateData() }
+    );
+  }
+
+  ngOnDestroy() {
+    this.updateSubscription.unsubscribe();
+  }
+
+  async updateData() {
     try {
       this.sensorData = await this.sensorDataService.list();
+      // Update your charts here with the new data
+      this.updateCarbonChart();
+      this.updateMethaneChart();
     } catch (error) {
       console.error(error);
     }
   }
-  methaneChart = new Chart({
-    chart: {
-      type: 'pie',
-      backgroundColor: '#6096B4',
-      style: {
-        fontFamily: 'Lucida'
-      }
+
+  setupGaugeCharts() {
+    // Create a gauge chart for carbon
+    this.carbonChart = new Chart({
+      chart: {
+        type: 'solidgauge',
+        backgroundColor: '#040D12'
+    
+        
+      },
+      credits: {
+        enabled: false
     },
-    title: {
-      text: 'Methane Data',
-      style: {
-        color: '#ffffff',
-        fontSize: '15px'
-      }
-    },
-    credits: {
-      enabled: false
-    },
-    plotOptions: {
-      pie: {
-        innerSize: '70%', // This makes the pie chart a donut chart
+      title: {
+        text: 'Carbon Concentration',
+        style: {
+          fontSize: '24px',
+          fontWeight: '100',
+          color: '#ffffff',
+          fontFamily: 'system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Open Sans, Helvetica Neue, sans-serif'
+        }
+      },
+      pane: {
         startAngle: -90,
         endAngle: 90,
-        center: ['50%', '75%'],
-        dataLabels: {
-          enabled: true
-        }
-      }
-    },
-    series: [
-      {
-        name: 'Methane',
-        data: [
-          {
-            name: 'Methane',
-            y: 56,
-            color: '#22092C'
-          }
-        ],
-        size: '130%',
-        innerSize: '70%'
-      }
-    ] as any
-  });
+        size: '140%',
+        center: ['50%', '85%'],
+        
   
-  carbonChart = new Chart({
-    chart: {
-      type: 'pie',
-      backgroundColor: '#6096B4',
-      style: {
-        fontFamily: 'Lucida'
-      }
+
+      },        
+      yAxis: {
+        min: 0,
+        max: 1,  
+        tickPosition: 'outside',
+        lineColor: '#000000',
+        lineWidth: 2,
+        labels: {
+          distance: 12,
+          rotation: 360
+        },
+        offset: -20,
+        endOnTick: true,
+        stops: [
+          [0.1, '#1A5D1A'], // green at 10%
+          [0.5, '#F4CE14'], // yellow at 50%
+          [0.9, '#B31312'] 
+        ]
+      },
+      series: [{  
+        name: 'Carbon',
+        data: [0.7],
+        dataLabels: {
+
+          backgroundColor: {
+            linearGradient: {
+              x1: 2,
+              y1: 3,
+              x2: 4,
+              y2: 1
+            },
+            stops: [
+              [0.1, '#040D12'], 
+               
+            ]
+          } 
+        },
+        tooltip: {
+          valueSuffix: '% CO2'
+        }
+        
+      }] as any
+    });
+    
+    
+      // ... other chart options for carbon
+  
+
+    // Create a gauge chart for methane
+    this.methaneChart = new Chart({
+      chart: {
+        type: 'solidgauge',
+        backgroundColor: '#040D12',
+        
+    
+        
+      },
+      credits: {
+        enabled: false
     },
-    title: {
-      text: 'Carbon (IV) Oxide Data',
-      style: {
-        color: '#ffffff',
-        fontSize: '15px'
-      }
-    },
-    credits: {
-      enabled: false
-    },
-    plotOptions: {
-      pie: {
-        innerSize: '170%', // This makes the pie chart a donut chart
+      
+      title: {
+        text: 'Methane Concentration',
+        style: {
+          fontSize: '24px',
+          fontWeight: '100',
+          color: '#ffffff',
+          fontFamily: 'system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Open Sans, Helvetica Neue, sans-serif'
+        }
+      },
+      pane: {
         startAngle: -90,
         endAngle: 90,
-        center: ['50%', '75%'],
+        size: '140%',
+        center: ['50%', '85%'],
+        
+  
+
+      },        
+      yAxis: {
+        min: 0,
+        max: 1,  
+        tickPosition: 'outside',
+        lineColor: '#000000',
+        lineWidth: 2,
+        labels: {
+          distance: 12,
+          rotation: 360
+        },
+        offset: -20,
+        endOnTick: true,
+        stops: [
+          [0.1, '#1A5D1A'], // green at 10%
+          [0.5, '#F4CE14'], // yellow at 50%
+          [0.9, '#B31312'] 
+        ]
+      },
+      series: [{  
+        name: 'Methane',
+        data: [0.8],
         dataLabels: {
-          enabled: true
+
+          backgroundColor: {
+            linearGradient: {
+              x1: 2,
+              y1: 3,
+              x2: 4,
+              y2: 1
+            },
+            stops: [
+              [0.1, '#040D12'], 
+               
+            ]
+          } 
+        },
+        tooltip: {
+          valueSuffix: '% CO2'
         }
-      }
-    },
-    series: [
-      {
-        name: 'Carbon (IV) oxide',
-        data: [
-          {
-            name: 'Carbon (IV) oxide',
-            y: 8,
-            color: '#323232'
-          }
-        ],
-        size: '130%',
-        innerSize: '70%'
-      }
-    ] as any
-  });
+        
+      }] as any
+    });
+  }
+
+  updateCarbonChart() {
+    // Update your carbon chart here with the new data
+  }
+
+  updateMethaneChart() {
+    // Update your methane chart here with the new data
+  }
 }
+
+
--- emissionInsight/src/app/app.module.ts
+++ emissionInsight/src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ChartModule } from 'angular-highcharts';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module'; 
 import { AppComponent } from './app.component';
 import { SensorDataService } from './aws-api.service';
+import {  HIGHCHARTS_MODULES } from 'angular-highcharts';
+import * as HighchartsMore from 'highcharts/highcharts-more.src';
+import * as HighchartsSolidGauge from 'highcharts/modules/solid-gauge';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -13,7 +17,7 @@ import { SensorDataService } from './aws-api.service';
     AppRoutingModule,
     ChartModule
   ],
-  providers: [SensorDataService],
+  providers: [SensorDataService, { provide: HIGHCHARTS_MODULES, useFactory: () => [HighchartsMore, HighchartsSolidGauge] }],
   bootstrap:  [AppComponent]
 })
 export class AppModule { }
